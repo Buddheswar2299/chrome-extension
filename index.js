bd55dbabfec47df00c642061905a3e5714b9bace
Buddheswar2299 <168357487+Buddheswar2299@users.noreply.github.com>
2024-05-01 10:49:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const inputEl = document.getElementById('input-el')
+const saveBtn = document.getElementById('save-btn')
+const storeValue = document.getElementById('store-value')
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
+let myLeads = []
+
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(myLeads)
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render()
+}
+console.log(myLeads)
+
+tabBtn.addEventListener('click',()=>{
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render()
+    })
+
+
+
+
+})
+
+
+
+
+
+saveBtn.addEventListener('click',()=>{
+
+    myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    inputEl.value = ''
+ 
+    
+
+    render()
+})
+function render(){
+    let renderme = ``
+    for(let i=0; i<myLeads.length; i++){
+        renderme += `<li> <a href="${myLeads[i]}" target=_blank> ${myLeads[i]} </a></li>`
+    }
+    storeValue.innerHTML = renderme
+}
+
+
+
+deleteBtn.addEventListener('click',()=>{
+    localStorage.clear()
+    myLeads = []
+    render()
+})
+
+
+
